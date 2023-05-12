55aaaeb88c8aca3288f1ed1d987c00ae0e98e7c5
Oakent <oakentravis@gmail.com>
2023-05-12 14:36:49 +0100

    solved 11

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  array = array.sort((a, b) => {
+    if (a.yearOfBirth < b.yearOfBirth) {
+      return -1;
+    }
+  });
+  return array[0];
 };
 
 // Do not edit below this line
